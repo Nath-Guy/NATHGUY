09890fc793c06b39029890dc2ba15db22281ffb3
nath <nathanaelle.guy@gmail.com>
2025-07-07 14:46:55 +0200

    Add Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Button from './components/ui/Button/Button'
+
 
 function App() {
 
   return (
-    <div>demat breizh</div>
+    <div>demat breizh
+    <Button></Button>
+    </div>
   )
 }
 
---
+++ src/components/ui/Button/Button.jsx
@@ -0,0 +1,6 @@
+const x=123;
+function Button(props) {
+    return <div className="Button">My Button</div>;
+}
+
+export default Button;
