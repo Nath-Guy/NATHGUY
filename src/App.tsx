97f1c9f16d46d5af7f72cba468f01fcd0273bd89
nath <nathanaelle.guy@gmail.com>
2025-07-08 15:05:29 +0200

    update on imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import FlexHorizontalFirstGrow from "./components/layout/FlexHorizontalFirstGrow/FlexHorizontalFirstGrow";
-import FlexVerticalThirdGrow from "./components/layout/FlexVerticalThirdGrow/FlexVerticalThirdGrow";
-import MemeForm from "./components/MemeForm/MemeForm";
-import Header from "./components/ui/Header/Header";
-import MemeSvgViewer from "./components/ui/MemeSVGViewer/MemeSVGViewer";
-import Navbar from "./components/ui/Navbar/Navbar";
+import FlexHorizontalFirstGrow from "./components/layout/FlexHorizontalFirstGrow/FlexHorizontalFirstGrow.tsx";
+import FlexVerticalThirdGrow from "./components/layout/FlexVerticalThirdGrow/FlexVerticalThirdGrow.tsx";
+import MemeForm from "./components/MemeForm/MemeForm.tsx";
+import Header from "./components/ui/Header/Header.tsx";
+import MemeSvgViewer from "./components/ui/MemeSVGViewer/MemeSVGViewer.tsx";
+import Navbar from "./components/ui/Navbar/Navbar.tsx";
 
 const App = () => {
   return (
