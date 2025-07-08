f04139584b1ba5bdebd7c53a26b04524eb9d2ee4
nath <nathanaelle.guy@gmail.com>
2025-07-08 15:17:47 +0200

    update header

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import styles from "./Header.module.css";
 
-
-
 const Header: React.FC = () => {
-
   return (
     <div className={styles.Header} data-testid="Header">
-      Header Component
+      <span className={styles.meme}>Meme</span>.<span className={styles.js}>js</span>
     </div>
   );
 };
