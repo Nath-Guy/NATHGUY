b5b5a8fe488ab67379691452a0c0a3c27aaf4dd8
nath <nathanaelle.guy@gmail.com>
2025-07-09 16:37:50 +0200

    add pages and router in main.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router";
 import App from "./App";
 import "./index.css";
 import { store } from "./store/store";
-import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </StrictMode>
 );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function home() {
+  return (
+    <div>
+      <h1>Bonjour</h1>
+      <p>créez vos memes personnalisés</p>
+      <p>
+            <a href='#'>New</a>
+            <a href='#'>Edit /0</a>
+            <a href='#'>Thumbnail</a>
+
+      </p>
+    </div>
+  );
+}
+
+export default home;
+
+
+
+
+//rfce
