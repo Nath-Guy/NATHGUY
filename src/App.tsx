887d07d33591be70b27e360b41dc101902fac879
nath <nathanaelle.guy@gmail.com>
2025-07-09 15:21:12 +0200

    meme form connected

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { emptyMeme, MemeSVGViewer, type ImageInterface } from "orsys-tjs-meme";
-import { useEffect, useState } from "react";
+import { emptyMeme } from "orsys-tjs-meme";
+import { useState } from "react";
 import "./App.css";
 import FlexHorizontalFirstGrow from "./components/layout/FlexHorizontalFirstGrow/FlexHorizontalFirstGrow.tsx";
 import FlexVerticalThirdGrow from "./components/layout/FlexVerticalThirdGrow/FlexVerticalThirdGrow.tsx";
 import MemeForm from "./components/MemeForm/MemeForm.connected.tsx";
+import MemeSVGViewer from "./components/MemeSVGViewer/MemeSVGViewer.connected.tsx";
 import Footer from "./components/ui/Footer/Footer.tsx";
 import Header from "./components/ui/Header/Header.tsx";
 import Navbar from "./components/ui/Navbar/Navbar.tsx";
@@ -12,16 +13,16 @@ import { store } from "./store/store.ts";
 const App = () => {
   const [meme, setMeme] = useState(emptyMeme);
 
-  //initial state of images : empty array
-  const [images, setImages] = useState<Array<ImageInterface>>([]);
+  // //initial state of images : empty array
+  // const [images, setImages] = useState<Array<ImageInterface>>([]);
 
   console.log(store);
 
-  useEffect(() => {
-    fetch("http://localhost:5679/images")
-      .then((r) => r.json())
-      .then((arr) => setImages(arr));
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:5679/images")
+  //     .then((r) => r.json())
+  //     .then((arr) => setImages(arr));
+  // }, []);
 
   return (
 
@@ -30,7 +31,7 @@ const App = () => {
           <Header></Header>
           <Navbar></Navbar>
           <FlexHorizontalFirstGrow>
-            <MemeSVGViewer image={undefined} meme={meme} />
+            <MemeSVGViewer />
             <MemeForm />
           </FlexHorizontalFirstGrow>
           <Footer></Footer>
